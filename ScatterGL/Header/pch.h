c2789ebafe002dffefefefdf7c95a4d52cf120e9
Gianluca <gianluca.piccardo@student.hu.nl>
2020-10-30 11:55:44 +0100

    Color on screen
    Added opening a GLFW window and giving it a color

---
+++ ScatterGL/Header/pch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdexcept>
+#include <iostream>
+#include <istream>
+#include <exception>
+#include <assert.h>
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
---
+++ ScatterGL/Source/main.cpp
@@ -0,0 +1,53 @@
+#include "pch.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+GLFWwindow* initWindow()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "ScatterGL", NULL, NULL);
+	if (window == NULL)
+	{
+		throw std::runtime_error("failed to create window \n");
+		glfwTerminate();
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		throw std::runtime_error("failed to initialize GLAD \n");
+	}
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	return window;
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+
+int main()
+{
+	GLFWwindow* window = initWindow();
+
+	while(!glfwWindowShouldClose(window))
+	{
+		//user input
+		processInput(window);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+}
