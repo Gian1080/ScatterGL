8f5e275b9e38403d542dc298fe5a9a57106b0f0b
Gianluca <gianluca.piccardo@student.hu.nl>
2020-11-10 15:48:55 +0100

    cleaning up before continueing

--- ScatterGL/Source/main.cpp
+++ ScatterGL/Source/main.cpp
@@ -36,24 +36,6 @@ GLFWwindow* initWindow()
 	return window;
 }
 
-const char* vertexShaderSource = "#version 460 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor\n;"
-"out vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
-"   ourColor = aColor;\n"
-"}\0";
-
-const char* fragmentShaderSource = "#version 460 core\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"    FragColor = vec4(ourColor, 1.0); \n"
-"}\0";
-
 int main()
 {
 	GLFWwindow* window = initWindow();
@@ -61,7 +43,6 @@ int main()
 	someShader.initialize("Shaders\\VertexShader.vert",
 							"Shaders\\FragmentShader.frag");
 
-	//glUseProgram(shaderProgram);
 	float verticesSquare[] = {
 	 0.5f,  0.5f, 0.0f,  // top right
 	 0.5f, -0.5f, 0.0f,  // bottom right
