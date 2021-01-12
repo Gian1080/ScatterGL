148a99a9e56fe24994a18a2dfe1bd9786fae15bc
Gianluca <gianluca.piccardo@student.hu.nl>
2021-01-12 15:19:04 +0100

    added many glad includes
    (not my code) :-1:

--- ScatterGL/Source/main.cpp
+++ ScatterGL/Source/main.cpp
@@ -214,6 +214,7 @@ int main()
 		info.lastFrame = currentFrame;
 		// user input
 		processInput(window, info);
+
 		// render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
